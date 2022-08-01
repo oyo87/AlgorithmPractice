d3a8334cf7cd15db78b72b16c4335fafa300703d
baebaejh <90134585+oyo87@users.noreply.github.com>
2022-08-02 00:28:09 +0900

    [Silver IV] Title: 한수, Time: 108 ms, Memory: 12948 KB -BaekjoonHub

--- 백준/Silver/1065. 한수/한수.java
+++ 백준/Silver/1065. 한수/한수.java
@@ -6,26 +6,26 @@ public class Main {
 
 		int N = sc.nextInt();
 
+		if (N < 100) {
+			System.out.println(N);
+			return;
+		}
 		int hundred = 0;
 		int ten = 0;
 		int one = 0;
-		int count = 0;
-		for (int i = 1; i <= N; i++) {
-			if (i < 100) {
+		int count = 99;
+		for (int i = 100; i <= N; i++) {
+			int temp = i;
+			hundred = temp / 100;
+			temp %= 100;
+			ten = temp / 10;
+			one = temp % 10;
+
+			if (hundred - ten == ten - one)
 				count++;
-			} else {
-				int temp = i;
-				hundred = temp / 100;
-				temp %= 100;
-				ten = temp / 10;
-				temp %= 10;
-				one = temp;
-
-				if (hundred - ten == ten - one)
-					count++;
-			}
-
 		}
+
 		System.out.println(count);
 	}
+
 }
