48f049007dad14721798928803afd260b7955bd1
baebaejh <90134585+oyo87@users.noreply.github.com>
2022-08-24 17:28:10 +0900

    [D2] Title: 숫자 배열 회전, Time: 144 ms, Memory: 21,468 KB -BaekjoonHub

--- SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.java
+++ SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Solution {
 	static int N;
 
-	static int[][][] Rotation(int[][][] arr) {//90도씩 회전을 해서 담아준다.
+	static int[][][] Rotation(int[][][] arr) {// 90도씩 회전을 해서 담아준다.
 		for (int t = 1; t <= 3; t++) {
 			for (int i = 0; i < N; i++) {
 				for (int j = 0; j < N; j++) {
@@ -24,24 +24,20 @@ public class Solution {
 			System.out.println("#" + tc);
 			N = sc.nextInt();
 
-			int[][][] arr = new int[4][N][N];//처음칸0이 원본 1이 90도 2가 180도 3이 270도를 담아둔다.
+			int[][][] arr = new int[4][N][N];// 처음칸0이 원본 1이 90도 2가 180도 3이 270도를 담아둔다.
 
 			for (int i = 0; i < N; i++) {
 				for (int j = 0; j < N; j++) {
 					arr[0][i][j] = sc.nextInt();
 				}
-			}//원본0에 입력받음
-
-			for (int i = 1; i <= N; i++) {
-				arr = Rotation(arr);
-			}
-
+			} // 원본0에 입력받음
+			arr = Rotation(arr);
 			for (int i = 0; i < N; i++) {
 				for (int j = 1; j <= 3; j++) {
 					for (int k = 0; k < N; k++) {
 						System.out.print(arr[j][i][k]);
 					}
-					if(j!=3)// 마지막에는 공백 구분을 할 필요가 없다.
+					if (j != 3)// 마지막에는 공백 구분을 할 필요가 없다.
 						System.out.print(" ");
 				}
 				System.out.println();
