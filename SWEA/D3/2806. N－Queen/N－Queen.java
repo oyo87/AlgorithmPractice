510dbb05beed425aab1a6d7c9fdc0ffa87ca3d4f
baebaejh <90134585+oyo87@users.noreply.github.com>
2022-09-22 14:32:00 +0900

    [D3] Title: N-Queen, Time: 143 ms, Memory: 33,136 KB -BaekjoonHub

---
+++ SWEA/D3/2806. N－Queen/N－Queen.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class Solution {
+
+	static int N;
+	static boolean[][] chessBoard;
+	static int possible;
+
+	public static boolean putPossible(int row, int col) {// 좌상,우상,위 쪽에 퀸이 이미 존재하면 불가능
+		int dia = 1;// diagonal 대각선
+
+		for (int i = row - 1; 0 <= i; i--) {
+			if (0 <= col - dia && chessBoard[i][col - dia]) {// 좌상향
+				return false;
+			}
+			if (col + dia < N && chessBoard[i][col + dia]) {// 우상향
+				return false;
+			}
+			if (chessBoard[i][col]) {
+				return false;
+			}
+			dia++;
+		}
+		return true;
+	}
+
+	public static void putQueen(int row) {
+		if (row == N) {
+			possible++;
+			return;
+		}
+		for (int col = 0; col < N; col++) {
+			if (putPossible(row, col)) {
+				chessBoard[row][col] = true;
+				putQueen(row + 1);
+				chessBoard[row][col] = false;
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int T = sc.nextInt();
+		for (int tc = 1; tc <= T; tc++) {
+			N = sc.nextInt();
+			possible = 0;
+			chessBoard = new boolean[N][N];
+			putQueen(0);
+			System.out.println("#" + tc + " " + possible);
+		}
+	}
+}
