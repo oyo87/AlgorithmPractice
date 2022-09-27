1cf967cc808e1b0b6548784d9ef31c92dbd96a2b
baebaejh <90134585+oyo87@users.noreply.github.com>
2022-09-27 11:17:39 +0900

    [D4] Title: 치즈 도둑, Time: 422 ms, Memory: 105,448 KB -BaekjoonHub

--- SWEA/D4/7733. 치즈 도둑/치즈 도둑.java
+++ SWEA/D4/7733. 치즈 도둑/치즈 도둑.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 //입력받고
 //day마다 day==면 0으로
-//0,0부터 0이 아닌것들중에 visit안한곳 탐색, 종료되면 덩어리count++
+//0,0부터 0이 아닌것들중에 visit안한곳 탐색, 종료되면 덩어리tempResult++
 public class Solution {
 	static int N;
 	static int[][] map;
@@ -25,13 +25,13 @@ public class Solution {
 			for (int i = 0; i < N; i++) {
 				for (int j = 0; j < N; j++) {
 					map[i][j] = sc.nextInt();
-					maxTaste = Math.max(maxTaste, map[i][j]);// maxTaste날까지만 계산하면 된다.
+					maxTaste = Math.max(maxTaste, map[i][j]);// day를 maxTaste날까지만 계산하면 된다.
 				}
 			}
 
 			visit = new boolean[N][N];
 			day = 1;
-			maxResult = 1;// 최대덩어리
+			maxResult = 1;// 최대덩어리 기본값은 1
 			tempResult = 0;
 			while (day <= maxTaste) {
 				for (int i = 0; i < N; i++) {
@@ -39,11 +39,11 @@ public class Solution {
 						if (map[i][j] == day)
 							map[i][j] = 0;
 					}
-				} // day에 맞게 0으로 맞춰주고
+				} // day와 동일한 맛은 0으로 설정
 
 				for (int i = 0; i < N; i++) {
 					for (int j = 0; j < N; j++) {
-						if (!visit[i][j] && map[i][j]!=0) {
+						if (!visit[i][j] && map[i][j] != 0) {
 							Recursion(i, j);
 							tempResult++;
 							maxResult = Math.max(maxResult, tempResult);
@@ -61,13 +61,13 @@ public class Solution {
 	}
 
 	static void Recursion(int x, int y) {
-		if (map[x][y]==0 ||visit[x][y]) {
+		if (visit[x][y]) {
 			return;
 		}
 
 		visit[x][y] = true;
 		for (int i = 0; i < 4; i++) {
-			if (rangeCheck(x, y, i) && 0 < map[x + delta[i][0]][y + delta[i][1]]) {//이동할곳이 0보다크면 이동
+			if (rangeCheck(x, y, i) && 0 < map[x + delta[i][0]][y + delta[i][1]]) {// 이동할곳이 0보다크면 이동
 				Recursion(x + delta[i][0], y + delta[i][1]);
 			}
 		}
