4e8be449b1dce623eb82d4f942f7b3c326b6aca1
baebaejh <90134585+oyo87@users.noreply.github.com>
2023-02-24 15:41:51 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 80 ms, Memory: 11828 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
@@ -4,60 +4,44 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
-    static Map<Integer, Integer> map = new HashMap<>();
-    static boolean[] visited;
-    static int N, M;
-
     public static void main(String[] args) throws IOException {
-        st = new StringTokenizer(br.readLine());
-        visited = new boolean[101];
-
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        Map<Integer, Integer> map = new HashMap<>();
+        boolean[] visited = new boolean[101];
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
         for (int i = 0; i < N + M; i++) { // 사다리, 뱀관련 이벤트위치를 map에 넣어줌 (위치, 이동할지점)
             st = new StringTokenizer(br.readLine());
             map.put(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
         }
 
-        bfs();
-    }   //  main-end
-
-    static void bfs() {
         Queue<Integer[]> q = new LinkedList<>();
         q.add(new Integer[]{1, 0});//시작
-        visited[1] = true;
-
         firstLoop:
         while (!q.isEmpty()) {
             Integer[] poll = q.poll();
-            int curPos = poll[0];   //  현재 위치
-            int curCnt = poll[1];   //  현재 위치까지 오는데 주사위 던진 횟수
-            int nextPos;                    //  다음 위치
-            int nextCnt =  curCnt + 1;      //  다음 위치까지 가는데 주사위 던진 횟수
 
-            if(map.containsKey(curPos)) {
-                curPos = map.get(curPos);
-                visited[curPos] = true;
+            if (map.containsKey(poll[0])) { // 현재위치가 사다리, 뱀이있다면 이동
+//                if (!visited[map.get(poll[0])]) {
+                    poll[0] = map.get(poll[0]);
+                    visited[poll[0]] = true;
+//                }
             }
-
-            if(curPos == 100) {
-                System.out.println(curCnt);
+            if (poll[0] == 100) { // 만약 현재위치가 100이라면 출력하고 끝내기
+                System.out.println(poll[1]);
                 return;
             }
 
-            for(int d = 1; d <= 6; d++) {
-                nextPos = curPos + d;   //  주사위 던졌을 때 갈 수 있는 칸
-                if(nextPos > 100) break;
-
-                if(visited[nextPos])    //  이미 방문한 칸일 경우
-                    continue;
-
-                visited[nextPos] = true;
-                q.offer(new Integer[]{nextPos, nextCnt});
+            for (int i = 1; i <= 6; i++) { // 1부터 6까지 차례대로 넣고 주사위 굴림, 100초과하면 멈춤
+                if (100 < poll[0] + i)
+                    continue firstLoop;
+                if (!visited[poll[0] + i]) {
+                    q.add(new Integer[]{poll[0] + i, poll[1] + 1});
+                    visited[poll[0] + i] = true;
+                }
             }
-        }   //  while-end
-    }   // bfs-end
+        }
+    }
 }
