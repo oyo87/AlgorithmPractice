2cf73528b25375afd8c64df50f69ea0f7feda233
baebaejh <90134585+oyo87@users.noreply.github.com>
2023-07-31 19:52:45 +0900

    [level 2] Title: 단체사진 찍기, Time: 1282.92 ms, Memory: 101 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/1835. 단체사진 찍기/단체사진 찍기.java
@@ -0,0 +1,69 @@
+import java.util.*;
+
+class Solution {
+    
+    String[] name = {"A", "C", "F", "J", "M", "N", "R", "T"};
+    String[] gData;//global Data
+    int answer = 0;
+    public int solution(int n, String[] data) {
+        /*
+        케이스수 8명 = 40400
+        모든 조합후 조건 검색해보자
+        */
+        gData = data;
+        boolean[] visited = new boolean[name.length];
+        String[] arr = new String[8];
+        recursion(arr, visited, 0);
+        
+        return answer;
+    }
+    
+    void recursion(String[] arr, boolean[]visited, int depth){
+        if(depth == 8){
+            //data 조건 검색
+            for(int i=0; i< gData.length; i++){
+                char me = gData[i].charAt(0);
+                char you = gData[i].charAt(2);
+                char sign = gData[i].charAt(3);
+                int dist = gData[i].charAt(4) - '0' + 1;//0일경우 1칸 이내 있어야함 +1
+                int meIndex = 0;
+                int youIndex = 0;
+                int diff = 0; // 실제 거리
+                
+                for(int j=0; j<arr.length; j++){
+                    if(arr[j].charAt(0) == me){
+                        meIndex = j;
+                    }
+                    else if (arr[j].charAt(0) == you){
+                        youIndex = j;
+                    }
+                }
+                diff = Math.abs(meIndex - youIndex);
+                
+                if(sign == '='){
+                    if(diff != dist)
+                        return ; 
+                }else if (sign == '<'){
+                    if(dist <= diff)
+                        return ;
+                }else{
+                    if(diff <= dist)
+                        return ;
+                }
+            }// for end
+            
+            answer++;
+            return ;
+        }
+        
+        //조합
+        for(int i=0; i<name.length; i++){
+            if(!visited[i]){
+                visited[i] = true;
+                arr[depth] = name[i];
+                recursion(arr, visited, depth + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
