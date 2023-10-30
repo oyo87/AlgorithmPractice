09e7bef94d4421ebaf02deeb734ee3f3e7718c37
baebaejh <90134585+oyo87@users.noreply.github.com>
2023-10-30 22:00:44 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 2512 ms, Memory: 439604 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
@@ -1,10 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -13,26 +10,17 @@ public class Main {
         Q에 적용할 연산의 개수 K : 0~100만
         I 숫자 : 숫자 삽입, D 1 최대값 삭제, D -1 최소값삭제
 
-        최소힙, 최대힙 두개 생성, map생성
-
-        I, D구현
-
-        map key 돌면서
-        max, min 출력
+        TreeMap활용한 편리한 풀이도 가능
          */
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         int T = Integer.parseInt(br.readLine());
-        PriorityQueue<Integer> min;
-        PriorityQueue<Integer> max;
-        HashMap<Integer, Integer> map;
+        TreeMap<Integer, Integer> map;
         StringBuilder sb = new StringBuilder();
 
         for (int tc = 0; tc < T; tc++) {
             int Q = Integer.parseInt(br.readLine());
-            min = new PriorityQueue<>();
-            max = new PriorityQueue<>(Collections.reverseOrder());
-            map = new HashMap<>();
+            map = new TreeMap<>();
             for (int i = 0; i < Q; i++) {
                 st = new StringTokenizer(br.readLine());
 
@@ -40,8 +28,6 @@ public class Main {
                 int number = Integer.parseInt(st.nextToken());// num, 1, -1
 
                 if (command == 'I') {
-                    min.add(number);
-                    max.add(number);
                     map.put(number, map.getOrDefault(number, 0) + 1);
                 } else { //command == 'D'
                     if (map.isEmpty()) {
@@ -49,37 +35,17 @@ public class Main {
                     }
 
                     if (number == 1) {//최대값 삭제
-                        int maxValue = max.poll();
-                        int count = map.getOrDefault(maxValue, 0);
-                        while (count == 0) {
-                            map.remove(maxValue);
-                            maxValue = max.poll();
-                            count = map.getOrDefault(maxValue, 0);
-                        }
-
-                        if (count == 1) {
-                            map.remove(maxValue);
-                        } else {
-                            map.put(maxValue, count - 1);
-                        }
-
-
+                        int max = map.lastKey();
+                        if (map.get(max) == 1)
+                            map.remove(max);
+                        else
+                            map.put(max, map.get(max) - 1);
                     } else {// number == -1 최소값 삭제
-
-                        int minValue = min.poll();
-                        int count = map.getOrDefault(minValue, 0);
-
-                        while (count == 0) {
-                            map.remove(minValue);
-                            minValue = min.poll();
-                            count = map.getOrDefault(minValue, 0);
-                        }
-
-                        if (count == 1) {
-                            map.remove(minValue);
-                        } else {
-                            map.put(minValue, count - 1);
-                        }
+                        int min = map.firstKey();
+                        if (map.get(min) == 1)
+                            map.remove(min);
+                        else
+                            map.put(min, map.get(min) - 1);
                     }
                 }
             }//Q for end
@@ -87,14 +53,7 @@ public class Main {
             if (map.isEmpty())
                 sb.append("EMPTY");
             else {
-                int maxValue = Integer.MIN_VALUE;
-                int minValue = Integer.MAX_VALUE;
-                for (int key : map.keySet()) {
-                    maxValue = Math.max(maxValue, key);
-                    minValue = Math.min(minValue, key);
-                }
-
-                sb.append(maxValue).append(" ").append(minValue);
+                sb.append(map.lastKey()).append(" ").append(map.firstKey());
             }
             sb.append("\n");
         }//tc end
